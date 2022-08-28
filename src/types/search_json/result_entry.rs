d389f943426d82acd8fdf1c90d2e8dd1eea0947f
adumbidiot <nathaniel.daniel23@outlook.com>
2022-08-28 16:12:39 -0700

    Replace `String` with `Box<str>`

--- src/types/search_json/result_entry.rs
+++ src/types/search_json/result_entry.rs
@@ -11,26 +11,26 @@ pub struct ResultEntry {
 
     /// Extra K/Vs
     #[serde(flatten)]
-    pub extra: HashMap<String, serde_json::Value>,
+    pub extra: HashMap<Box<str>, serde_json::Value>,
 }
 
 /// A result entry header
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Header {
     /// Result similarity
-    pub similarity: String,
+    pub similarity: Box<str>,
     /// Image thumbnail
     pub thumbnail: Url,
     /// index id?
     pub index_id: u64,
     /// The index name
-    pub index_name: String,
+    pub index_name: Box<str>,
     /// the # of dupes
     pub dupes: u64,
 
     /// Extra K/Vs
     #[serde(flatten)]
-    pub extra: HashMap<String, serde_json::Value>,
+    pub extra: HashMap<Box<str>, serde_json::Value>,
 }
 
 /// Result data
@@ -41,13 +41,13 @@ pub struct Data {
     pub ext_urls: Vec<Url>,
     /// title
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub title: Option<String>,
+    pub title: Option<Box<str>>,
     /// Deviantart ID
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub da_id: Option<String>,
+    pub da_id: Option<Box<str>>,
     /// Author name
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub author_name: Option<String>,
+    pub author_name: Option<Box<str>>,
     /// Author URL
     #[serde(skip_serializing_if = "Option::is_none")]
     pub author_url: Option<Url>,
@@ -56,49 +56,49 @@ pub struct Data {
     pub pixiv_id: Option<u64>,
     /// member name
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub member_name: Option<String>,
+    pub member_name: Option<Box<str>>,
     /// member id
     #[serde(skip_serializing_if = "Option::is_none")]
     pub member_id: Option<u64>,
     /// source
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub source: Option<String>,
+    pub source: Option<Box<str>>,
     /// ?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub anidb_aid: Option<u64>,
     ///?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub part: Option<String>,
+    pub part: Option<Box<str>>,
     /// anime year?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub year: Option<String>,
+    pub year: Option<Box<str>>,
     /// ?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub est_time: Option<String>,
+    pub est_time: Option<Box<str>>,
     /// fur affinity id
     #[serde(skip_serializing_if = "Option::is_none")]
     pub fa_id: Option<u64>,
     /// twitter tweet creation date
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub created_at: Option<String>,
+    pub created_at: Option<Box<str>>,
     /// tweet_id
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub tweet_id: Option<String>,
+    pub tweet_id: Option<Box<str>>,
     /// twitter_user_id
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub twitter_user_id: Option<String>,
+    pub twitter_user_id: Option<Box<str>>,
     /// twitter_user_handle
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub twitter_user_handle: Option<String>,
+    pub twitter_user_handle: Option<Box<str>>,
     /// creator?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub creator: Option<Creator>,
     /// eng name?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub eng_name: Option<String>,
+    pub eng_name: Option<Box<str>>,
     /// jp name?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub jp_name: Option<String>,
+    pub jp_name: Option<Box<str>>,
     /// bcy_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub bcy_id: Option<u64>,
@@ -107,25 +107,25 @@ pub struct Data {
     pub member_link_id: Option<u64>,
     /// bcy_type?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub bcy_type: Option<String>,
+    pub bcy_type: Option<Box<str>>,
     /// fn_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub fn_id: Option<u64>,
     /// fn_type?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub fn_type: Option<String>,
+    pub fn_type: Option<Box<str>>,
     /// pawoo_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub pawoo_id: Option<u64>,
     /// pawoo_user_acct?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub pawoo_user_acct: Option<String>,
+    pub pawoo_user_acct: Option<Box<str>>,
     /// pawoo_user_username?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub pawoo_user_username: Option<String>,
+    pub pawoo_user_username: Option<Box<str>>,
     /// pawoo_user_display_name?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub pawoo_user_display_name: Option<String>,
+    pub pawoo_user_display_name: Option<Box<str>>,
     /// seiga_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub seiga_id: Option<u64>,
@@ -134,10 +134,10 @@ pub struct Data {
     pub danbooru_id: Option<u64>,
     /// material?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub material: Option<String>,
+    pub material: Option<Box<str>>,
     /// characters?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub characters: Option<String>,
+    pub characters: Option<Box<str>>,
     /// konachan_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub konachan_id: Option<u64>,
@@ -152,17 +152,17 @@ pub struct Data {
     pub sankaku_id: Option<u64>,
     /// artist?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub artist: Option<String>,
+    pub artist: Option<Box<str>>,
     /// author?
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub author: Option<String>,
+    pub author: Option<Box<str>>,
     /// md_id?
     #[serde(skip_serializing_if = "Option::is_none")]
     pub md_id: Option<Box<str>>,
 
     /// Extra K/Vs
     #[serde(flatten)]
-    pub extra: HashMap<String, serde_json::Value>,
+    pub extra: HashMap<Box<str>, serde_json::Value>,
 }
 
 /// The creator field of [`Data`]
@@ -170,8 +170,8 @@ pub struct Data {
 #[serde(untagged)]
 pub enum Creator {
     /// a single creator
-    Single(String),
+    Single(Box<str>),
 
     /// multiple creators
-    Multiple(Vec<String>),
+    Multiple(Vec<Box<str>>),
 }
